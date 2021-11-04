10835691a08a66503d83fd2d303a6e409648faa7
Roman Potemin <poteminr@gmail.com>
2021-11-04 20:43:41 +0700

    add notebook and script for NER

--- NER/spans_prediction.py
+++ NER/spans_prediction.py
@@ -1,13 +1,40 @@
 import re
 from span_words import span_words
+from transformers import AutoModelForTokenClassification, AutoTokenizer, pipeline
+
+class NER:
+    def __init__(self, path_to_model, path_to_tokenizer, device=0):
+        self.path_to_model = path_to_model
+        self.path_to_tokenizer = path_to_tokenizer
+        self.device = device
+
+        self.model = AutoModelForTokenClassification.from_pretrained(self.path_to_model)
+        self.tokenizer = AutoTokenizer.from_pretrained(self.path_to_tokenizer)
+
+        self.hugginface_pipeline = pipeline(model=self.model, tokenizer=self.tokenizer, task='ner', 
+                                            aggregation_strategy='average', device=self.device)
+
+                                            
+    def predict_spans_baseline(self, text, span_length_threshold=3):
+        # Just finds words from span_words in the given string
+        spans = []
+        for word in span_words:
+            if len(word) > span_length_threshold:
+                for match in re.finditer(word, text):
+                    spans.append([match.span()[0], match.span()[1]])
+                
+        return spans
 
 
-def predict_spans_baseline(string, span_length_threshold=3):
-    # Just finds words from span_words in the given string
-    spans = []
-    for word in span_words:
-        if len(word) > span_length_threshold:
-            for match in re.finditer(word, string):
-                spans.append([match.span()[0], match.span()[1]])
-            
-    return spans
+    def predict_spans(self, text):
+        spans = []
+        prediction = self.hugginface_pipeline(text)
+
+        if len(spans) == 0:
+            spans = self.predict_spans_baseline(text)
+
+        else:
+            for entity in prediction:
+                spans.append([entity['start'], entity['end']])
+        
+        return spans
